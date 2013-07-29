6f7b5851f09d53ea771b11114bab9b422f9bd1fd
cloud2081 <cloud2081@65689a98-b01c-40b5-8ba4-364e7b6acab1>
2013-07-29 13:40:30 +0000

    Spider

---
+++ branches/ConsoleApplication1/ConsoleApplication1/DatabaseController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    public class DatabaseController
+    {
+        private projectEntities db = new projectEntities();
+
+        public projectEntities Database // ok
+        {
+            get
+            {
+                return db;
+            }
+        }
+
+        public bool InsertViewType(viewtype insertViewType) //ok
+        {
+            List<viewtype> typeList = db.viewtypes.Where(o => o.Typename == insertViewType.Typename).ToList();
+            if (typeList.Count == 0)
+            {
+                db.viewtypes.Add(insertViewType);
+                db.SaveChanges();
+                return true;
+            }
+            else 
+            {
+                return false;
+            }
+        }
+
+        public void InsertCity(city insertCity) // ok
+        {
+            db.cities.Add(insertCity);
+            db.SaveChanges();
+        }
+
+        public int GetViewTypeNumber(string typeName) //ok
+        {
+            return db.viewtypes.Where(o => o.Typename == typeName).First().Typenumber;
+        }
+
+        public void InsertPlace(place insertPlace)
+        {
+            db.places.Add(insertPlace);
+            db.SaveChanges();
+        }
+
+        public void InsertPlaceImage(placeimage insertImage) //
+        {
+            db.placeimages.Add(insertImage);
+            db.SaveChanges();
+        }
+    }
+}
---
+++ branches/ConsoleApplication1/ConsoleApplication1/ImageConverter.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.IO;
+
+namespace ConsoleApplication1
+{
+    /// <summary>
+    /// Class contaning method to resize an image and save in JPEG format
+    /// </summary>
+    public class ImageConverter
+    {
+        /// <summary>
+        /// Method to resize, convert and save the image.
+        /// </summary>
+        /// <param name="image">Bitmap image.</param>
+        /// <param name="maxWidth">resize width.</param>
+        /// <param name="maxHeight">resize height.</param>
+        /// <param name="quality">quality setting value.</param>
+        /// <param name="filePath">file path.</param>      
+        public static Image ResizeImage(Bitmap image, int maxWidth, int maxHeight, int quality)
+        {
+            // Get the image's original width and height
+            int originalWidth = image.Width;
+            int originalHeight = image.Height;
+
+            // To preserve the aspect ratio
+            float ratioX = (float)maxWidth / (float)originalWidth;
+            float ratioY = (float)maxHeight / (float)originalHeight;
+            float ratio = Math.Min(ratioX, ratioY);
+
+            // New width and height based on aspect ratio
+            int newWidth = (int)(originalWidth * ratio);
+            int newHeight = (int)(originalHeight * ratio);
+
+            // Convert other formats (including CMYK) to RGB.
+            Bitmap newImage = new Bitmap(newWidth, newHeight, PixelFormat.Format24bppRgb);
+
+            // Draws the image in the specified size with quality mode set to HighQuality
+            using (Graphics graphics = Graphics.FromImage(newImage))
+            {
+                graphics.CompositingQuality = CompositingQuality.HighQuality;
+                graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                graphics.SmoothingMode = SmoothingMode.HighQuality;
+                graphics.DrawImage(image, 0, 0, newWidth, newHeight);
+            }
+
+            // Get an ImageCodecInfo object that represents the JPEG codec.
+            ImageCodecInfo imageCodecInfo = GetEncoderInfo(ImageFormat.Jpeg);
+
+            // Create an Encoder object for the Quality parameter.
+            Encoder encoder = Encoder.Quality;
+
+            // Create an EncoderParameters object. 
+            EncoderParameters encoderParameters = new EncoderParameters(1);
+
+            // Save the image as a JPEG file with quality level.
+            EncoderParameter encoderParameter = new EncoderParameter(encoder, quality);
+            encoderParameters.Param[0] = encoderParameter;
+            // input parameter filepath
+            //newImage.Save(filePath, imageCodecInfo, encoderParameters);
+            return newImage;
+        }
+
+        /// <summary>
+        /// Method to get encoder infor for given image format.
+        /// </summary>
+        /// <param name="format">Image format</param>
+        /// <returns>image codec info.</returns>
+        private static ImageCodecInfo GetEncoderInfo(ImageFormat format)
+        {
+            return ImageCodecInfo.GetImageDecoders().SingleOrDefault(c => c.FormatID == format.Guid);
+        }
+
+        /// <summary>
+        /// 將 Image 轉換為 Byte 陣列。
+        /// </summary>
+        /// <param name="Image">Image 。</param>
+        /// <param name="imageFormat">指定影像格式。</param>        
+        public static byte[] ImageToBuffer(Image Image, System.Drawing.Imaging.ImageFormat imageFormat)
+        {
+            if (Image == null) { return null; }
+            byte[] data = null;
+            using (MemoryStream oMemoryStream = new MemoryStream())
+            {
+                //建立副本
+                using (Bitmap oBitmap = new Bitmap(Image))
+                {
+                    //儲存圖片到 MemoryStream 物件，並且指定儲存影像之格式
+                    oBitmap.Save(oMemoryStream, imageFormat);
+                    //設定資料流位置
+                    oMemoryStream.Position = 0;
+                    //設定 buffer 長度
+                    data = new byte[oMemoryStream.Length];
+                    //將資料寫入 buffer
+                    oMemoryStream.Read(data, 0, Convert.ToInt32(oMemoryStream.Length));
+                    //將所有緩衝區的資料寫入資料流
+                    oMemoryStream.Flush();
+                }
+            }
+            return data;
+        }
+
+        /// <summary>
+        /// 將 Byte 陣列轉換為 Image。
+        /// </summary>
+        /// <param name="Buffer">Byte 陣列。</param>        
+        public static Image BufferToImage(byte[] Buffer)
+        {
+            if (Buffer == null || Buffer.Length == 0) { return null; }
+            byte[] data = null;
+            Image oImage = null;
+            Bitmap oBitmap = null;
+            //建立副本
+            data = (byte[])Buffer.Clone();
+            try
+            {
+                MemoryStream oMemoryStream = new MemoryStream(Buffer);
+                //設定資料流位置
+                oMemoryStream.Position = 0;
+                oImage = System.Drawing.Image.FromStream(oMemoryStream);
+                //建立副本
+                oBitmap = new Bitmap(oImage);
+                //oImage.Save("output.jpg", ImageFormat.Jpeg);
+            }
+            catch
+            {
+                throw;
+            }
+            //return oImage;
+            return oBitmap;
+        }
+    }
+}
