314dbc08e08cc6a0c61e6255bb8c0848e5ddf1d6
kevin61225 <kevin61225@65689a98-b01c-40b5-8ba4-364e7b6acab1>
2013-09-10 10:18:11 +0000

    [新增] Views/Map/ViewPointDetails.cshtml 加入 "綜合評價" (須先加入 Rating Table 才能使用)

---
+++ EasyTravelInTaiwan/Controllers/RatingController.cs
@@ -0,0 +1,40 @@
+﻿using EasyTravelInTaiwan.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EasyTravelInTaiwan.Controllers
+{
+    public class RatingController : Controller
+    {
+        //
+        // GET: /Rating/
+        ProjectEntities db = new ProjectEntities();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult PlaceRatePartial(string sno)
+        {
+            int rate;
+
+            try
+            {
+                rate = db.ratings.Where(o => o.Sno == sno).Sum(o => o.Point);
+            }
+            catch
+            {
+                rate = -1;
+            }
+
+            ViewBag.RateSum = rate;
+
+            return PartialView("_placeRatingPartial");
+        }
+
+    }
+}
