9775bcf426f87f5a37ff68ba3786fbad59902c2e
kevin61225 <kevin61225@65689a98-b01c-40b5-8ba4-364e7b6acab1>
2013-09-25 10:12:54 +0000

    [修正] 補上 partialMember.cs & Direction.cshtml

---
+++ EasyTravelInTaiwan/Models/DatabaseConstructor/partialMember.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EasyTravelInTaiwan.Models
+{
+    public partial class member
+    {
+        public member(RegisterModel copiedMember)
+        {
+            this.travellists = new HashSet<travellist>();
+            this.ratings = new HashSet<rating>();
+
+            UserID = copiedMember.UserID;
+            Account = copiedMember.Account;
+            Password = copiedMember.Password;
+            Name = copiedMember.Name;
+            Email = copiedMember.Email;
+            PhoneNumber = "none";
+            Sex = copiedMember.Sex;
+            UserAddress = "none";
+            Birthday = copiedMember.Birthday;
+            favorite = string.Empty;
+            for (int i = 0; i < copiedMember.ViewTypeList.Count; i++)
+            {
+                if(copiedMember.ViewTypeList[i].isCheck)
+                {
+                    favorite += copiedMember.ViewTypeList[i].viewtype.Typenumber + "-";
+                }
+            }
+            favorite = favorite.Remove(favorite.Length-1);
+            Role = 2;
+        }
+    }
+}
